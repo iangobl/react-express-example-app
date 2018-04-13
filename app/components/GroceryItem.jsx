3e9b10ef200053e4741ce70b8116b1b305f113aa
Ian Gobl <iangobl@hotmail.com>
2018-04-13 12:50:04 +0100

    1. Adding styles to react application

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -5,11 +5,11 @@ var action = require('./../actions/GroceryItemActionCreator.jsx');
 module.exports = createReactClass({
     togglePurchased: function (e) {
         e.preventDefault();
-        
+
         if (this.props.item.purchased) {
             action.unbuy(this.props.item);
         } else {
-            action.buy(this.props.item);            
+            action.buy(this.props.item);
         }
     },
     delete: function (e) {
@@ -17,21 +17,22 @@ module.exports = createReactClass({
         action.delete(this.props.item);
     },
     render: function () {
-        return (<div>
-            <div>
-            <h4 className={this.props.item.purchased ? "strikethrough" : ""}>
-                {this.props.item.name}
-            </h4>
+        return (
+            <div className="grocery-item row">
+                <div className="six columns">
+                    <h4 className={this.props.item.purchased ? "strikethrough" : ""}>
+                        {this.props.item.name}
+                    </h4>
+                </div>
+                <form className="three columns" onSubmit={this.togglePurchased}>
+                    <button className={this.props.item.purchased ? "" : "button-primary"}>
+                        {this.props.item.purchased ? "UnBuy" : "Buy"}
+                    </button>
+                </form>
+                <form className="three columns" onSubmit={this.delete}>
+                    <button>&times;</button>
+                </form>
             </div>
-            <form className="three columns" onSubmit={this.togglePurchased}>
-                <button className={this.props.item.purchased ? "" : "button-primary"}>
-                    {this.props.item.purchased ? "UnBuy" : "Buy"}
-                </button>    
-            </form>
-            <form className="three columns" onSubmit={this.delete}>
-                <button>&times;</button>    
-            </form>
-        </div>
         );
     }
 });
