787a54d822e770cc28162de8e0bb9c1bcff4eae9
Ian Gobl <iangobl@hotmail.com>
2018-04-13 11:56:09 +0100

    1. Adding functionality to buy/unbuy items

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -3,6 +3,15 @@ var createReactClass = require('create-react-class');
 var action = require('./../actions/GroceryItemActionCreator.jsx');
 
 module.exports = createReactClass({
+    togglePurchased: function (e) {
+        e.preventDefault();
+        
+        if (this.props.item.purchased) {
+            action.unbuy(this.props.item);
+        } else {
+            action.buy(this.props.item);            
+        }
+    },
     delete: function (e) {
         e.preventDefault();
         action.delete(this.props.item);
@@ -14,6 +23,11 @@ module.exports = createReactClass({
                 {this.props.item.name}
             </h4>
             </div>
+            <form className="three columns" onSubmit={this.togglePurchased}>
+                <button className={this.props.item.purchased ? "" : "button-primary"}>
+                    {this.props.item.purchased ? "UnBuy" : "Buy"}
+                </button>    
+            </form>
             <form className="three columns" onSubmit={this.delete}>
                 <button>&times;</button>    
             </form>
